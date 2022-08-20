cdf875241ffa098362ea58b104ca50c791c23bf3
Kimlee-cmd <85122673+Kimlee-cmd@users.noreply.github.com>
2022-08-20 18:25:28 +0800

    Add files via upload

--- app/src/main/java/com/github/catvod/spider/Kmys.java
+++ app/src/main/java/com/github/catvod/spider/Kmys.java
@@ -44,9 +44,9 @@ public class Kmys extends Spider {
     private String apiDomain = "";
     private String staticDomain = "";
 
-    private String appId = "5"; // 飞瓜 1 酷猫 5
+    private String appId = "4"; // 飞瓜 1 酷猫 5
 
-    private String device = "1a7348ae45f8f6de4d798614fc07e7271657392501986";
+    private String device = "7436a1b89a626665225e8a54a5852f3a1660924816806";
 
     @Override
     public void init(Context context) {
@@ -434,10 +434,10 @@ public class Kmys extends Spider {
             HashMap hashMap = new HashMap();
             hashMap.put("versionNumber", "360");
             hashMap.put("versionName", "3.6.0");
-            hashMap.put("device", "1a7348ae45f8f6de4d798614fc07e7271657392501986");
+            hashMap.put("device", "7436a1b89a626665225e8a54a5852f3a1660924816806");
             //hashMap.put("appId", "5");
             hashMap.put("platformId", "7");
-            hashMap.put("User-Agent", "okhttp/3.14.7");
+            hashMap.put("User-Agent", "Dalvik/2.1.0");
             hashMap.put("Pragma", "Pragma:: no-cache");
             hashMap.put("Cache-Control", "no-cache");
             hashMap.put("Host", "mtv.stvmts.com");
@@ -551,7 +551,7 @@ public class Kmys extends Spider {
         Uri uri = Uri.parse(m3u8);
         String time = String.valueOf(System.currentTimeMillis());
         String key = Misc.MD5(subUrl(m3u8) + signPlayerStr + time, Misc.CharsetUTF8).toLowerCase();
-        String realUrl = m3u8 + "?key=" + key + "&v=360&i=5&p=7&ed=1a7348ae45f8f6de4d798614fc07e7271657392501986&time=" + time;
+        String realUrl = m3u8 + "?key=" + key + "&v=360&i=5&p=7&ed=7436a1b89a626665225e8a54a5852f3a1660924816806&time=" + time;
         String m3u8Content = OkHttpUtil.string(realUrl, kmysPlayerHeaders);
         String tsUrl = m3u8.substring(0, m3u8.indexOf(uri.getLastPathSegment()));
         StringBuilder sb = new StringBuilder();
@@ -581,7 +581,7 @@ public class Kmys extends Spider {
     static Object[] getKeyContent(String key) {
         String time = String.valueOf(System.currentTimeMillis());
         String md5 = Misc.MD5(subUrl(key) + signPlayerStr + time, Misc.CharsetUTF8).toLowerCase();
-        String realUrl = key + "?key=" + md5 + "&v=360&i=5&p=7&ed=1a7348ae45f8f6de4d798614fc07e7271657392501986&time=" + time;
+        String realUrl = key + "?key=" + md5 + "&v=360&i=5&p=7&ed=7436a1b89a626665225e8a54a5852f3a1660924816806&time=" + time;
         String keyContent = OkHttpUtil.string(realUrl, kmysPlayerHeaders);
         String type = "application/octet-stream";
         Object[] result = new Object[3];
@@ -595,7 +595,7 @@ public class Kmys extends Spider {
     static Object[] getTsContent(String ts) {
         String time = String.valueOf(System.currentTimeMillis());
         String key = Misc.MD5(subUrl(ts) + signPlayerStr + time, Misc.CharsetUTF8).toLowerCase();
-        String realUrl = ts + "?key=" + key + "&v=360&i=5&p=7&ed=1a7348ae45f8f6de4d798614fc07e7271657392501986&time=" + time;
+        String realUrl = ts + "?key=" + key + "&v=360&i=5&p=7&ed=7436a1b89a626665225e8a54a5852f3a1660924816806&time=" + time;
         OKCallBack.OKCallBackDefault callBack = new OKCallBack.OKCallBackDefault() {
 
 
@@ -631,7 +631,7 @@ public class Kmys extends Spider {
         if (kmysPlayerHeaders == null) {
             kmysPlayerHeaders = new HashMap<>();
             kmysPlayerHeaders.put("User-Agent", "okhttp/3.14.7");
-            kmysPlayerHeaders.put("ed", "1a7348ae45f8f6de4d798614fc07e7271657392501986");
+            kmysPlayerHeaders.put("ed", "7436a1b89a626665225e8a54a5852f3a1660924816806");
             kmysPlayerHeaders.put("Connection", "close");
         }
         if (type.equals("m3u8")) {
